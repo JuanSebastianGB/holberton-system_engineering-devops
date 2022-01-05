9d5026d5c58c38396ab817a7dd2b525ed579552a
JuanSebastianGB <3393@holbertonschool.com>
2022-01-05 21:27:40 +0000

    UPD: Uploading task 4 4. Client configuration file (w/ Puppet)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# Let’s practice using Puppet to make changes to our configuration file.
+# Just as in the previous configuration file task, we’d like you to set up
+# your client SSH configuration file so that you can connect to a server without typing a password.
+
+file_line { 'Use private key':
+path =>'/etc/ssh/ssh_config',
+line => 'IdentityFile ~/.ssh/school'
+}
+
+file_line { 'Refuse password':
+  path => 'etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no'
+}
