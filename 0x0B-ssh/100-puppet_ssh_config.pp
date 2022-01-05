e6b826026e5bd35abecb5e36e2f0e21c2e49c7f4
JuanSebastianGB <3393@holbertonschool.com>
2022-01-05 21:44:14 +0000

    UPD: Adding presetn in to private key and no password requirement

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,11 +3,13 @@
 # your client SSH configuration file so that you can connect to a server without typing a password.
 
 file_line { 'Use private key':
-path =>'/etc/ssh/ssh_config',
-line => 'IdentityFile ~/.ssh/school'
+ensure => present,
+path   =>'/etc/ssh/ssh_config',
+line   => 'IdentityFile ~/.ssh/school'
 }
 
 file_line { 'Refuse password':
-  path => 'etc/ssh/ssh_config',
-  line => 'PasswordAuthentication no'
+ensure => present,
+path   => 'etc/ssh/ssh_config',
+line   => 'PasswordAuthentication no'
 }
