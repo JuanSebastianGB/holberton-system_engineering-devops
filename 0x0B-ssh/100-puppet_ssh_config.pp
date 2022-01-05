eb4fd2e8ff3c4e78fc2d44a89a563ccd43ea9610
JuanSebastianGB <3393@holbertonschool.com>
2022-01-05 21:49:53 +0000

    FIX: Fixing path avoiding password

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,13 +3,13 @@
 # your client SSH configuration file so that you can connect to a server without typing a password.
 
 file_line { 'Use private key':
-ensure => present,
+ensure => 'present',
 path   =>'/etc/ssh/ssh_config',
 line   => 'IdentityFile ~/.ssh/school'
 }
 
-file_line { 'Refuse password':
-ensure => present,
-path   => 'etc/ssh/ssh_config',
+file_line { 'Avoid password':
+ensure => 'present',
+path   => '/etc/ssh/ssh_config',
 line   => 'PasswordAuthentication no'
 }
