7790be03208d7dfeda9a3fc700aa29be8e32d044
JuanSebastianGB <3393@holbertonschool.com>
2022-01-15 23:34:25 +0000

    UPD: Moving to test

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -7,9 +7,6 @@ exec { 'update':
     ensure  => installed,
     require => Exec['update']
 }
--> file { '/var/www/html/index.html':
-    content => 'Hello World',
-}
 
 -> file_line { 'adding header':
     ensure => 'present',
@@ -23,6 +20,10 @@ exec { 'update':
     path   => '/etc/nginx/sites-available/default',
     after  => 'listen 80 default_server;',
     line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+-> file { '/var/www/html/index.html':
+    content => 'Hello World',
 }
 
 -> service { 'nginx':
