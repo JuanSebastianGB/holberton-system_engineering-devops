1f09aee46b8aa56bcec542e5fbff6822aeef2e5d
JuanSebastianGB <3393@holbertonschool.com>
2022-01-15 22:46:29 +0000

    UPD: Adding missing semicolon

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -13,7 +13,7 @@ exec { 'update':
 
 -> file_line { 'adding header':
     ensure => 'present',
-    line   => "add_header X-Served-By ${hostname};"
+    line   => "add_header X-Served-By ${hostname};",
     path   => '/etc/nginx/sites-available/default',
     after  => 'listen 80 default_server;',
 }
@@ -22,7 +22,7 @@ exec { 'update':
     ensure => 'present',
     path   => '/etc/nginx/sites-available/default',
     after  => 'listen 80 default_server;',
-    line   => 'rewrite ^/redirect_me https://github.com/JuanSebastianGB/ permanent;',
+    line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 -> service { 'nginx':
