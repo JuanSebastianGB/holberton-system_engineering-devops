56bbdddeaaf50d9c572d3a15e13f7fb8792ee108
JuanSebastianGB <3393@holbertonschool.com>
2022-02-01 03:45:59 +0000

    UPL: Uploading task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Calling to an api and etting todos task of an specific user """
+import sys
+import requests
+
+if __name__ == '__main__':
+    user = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(sys.argv[1]),
+        verify=False).json()
+    todos = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userID={}'.format(
+            sys.argv[1]),
+        verify=False).json()
+
+    complete = [task.get('title')
+                for task in todos if task.get('completed')]
+    print(
+        'Employee {} is done with tasks({}/{}):'.
+        format(user['name'],
+               complete.__len__(),
+               todos.__len__()))
+    print("\n".join("\t {}".format(task) for task in complete))
