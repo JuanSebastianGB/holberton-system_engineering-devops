67e38aeaeb9e463e42ed4fa4a2c55b56e815415a
JuanSebastianGB <3393@holbertonschool.com>
2022-02-16 02:45:13 +0000

    DEL: Deleting unused param

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,25 +3,25 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[], count=0, _next=''):
+def recurse(subreddit, hot_list=[], count=0, after=''):
     """ queries the Reddit API and returns a list containing the titles
     of all hot articles for a given subreddit """
     agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0)'
     agent += ' Gecko/20100101 Firefox/47.0'
-    url = 'https://www.reddit.com/r/{}/hot/.json?limit=500&count={}&_next={}'\
+    url = 'https://www.reddit.com/r/{}/hot/.json?count={}&after={}'\
         .format(
-            subreddit, count, _next)
+            subreddit, count, after)
     headers = {"user-Agent": agent}
     response = requests.get(
         url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         return None
     results = response.json().get('data')
-    _next = results.get('after')
+    after = results.get('after')
     count += results.get('dist')
     children = results.get('children')
     [hot_list.append(result.get('data').get('title')) for result in children]
 
-    if _next:
+    if after:
         return hot_list
-    return recurse(subreddit, hot_list, _next, count)
+    return recurse(subreddit, hot_list, after, count)
