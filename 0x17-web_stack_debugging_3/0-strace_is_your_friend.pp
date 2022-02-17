e859a34e74ea90eef1b190e0888001b339e69020
JuanSebastianGB <3393@holbertonschool.com>
2022-02-17 03:03:25 +0000

    FIX: Adding header to the file and adding : after the description

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,6 @@
-exec { 'Changing word to fix 500 server error'
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+# fixes Apache 500 error by fixing typo in wordpress
+
+exec { 'Changing word to fix 500 server error':
+  command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   provider => shell,
 }
